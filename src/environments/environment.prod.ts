613b7da1756d04f11a8f6bcf255fe039b20de8f1
Nguyen Huu Hieu <hieunh1801@gmail.com>
2021-10-29 15:06:56 +0700

    (1): remove unuse environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,6 @@
 export const environment = {
   version: '1.0',
   host: 'https://portal.spmed.kr',
-  customerPortal: 'https://pcdss.spmed.kr',
-  genotypeCallingPortal: 'https://genocalling.spmed.kr',
   gateway: 'https://gwapi.spmed.kr/api-gateway/v1.0',
 
   logRequest: false, // log request and response config
