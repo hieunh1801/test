4da2782b4547cc99e99e876578b4a1d301be2f66
Nguyen Huu Hieu <hieunh1801@gmail.com>
2021-11-01 10:36:14 +0700

    (1): remove unused module (2) update development guide

--- src/app/pdss/pdss.module.ts
+++ src/app/pdss/pdss.module.ts
@@ -7,8 +7,6 @@ import { OverviewComponent } from './components/overview/overview.component';
 import { BrowserComponent } from './components/browser/browser.component';
 import { UtilityComponent } from './components/utility/utility.component';
 import { ProductsComponent } from './components/products/products.component';
-import { MaterialModule } from '../material.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { PgxComponent } from './components/products/components/pgx/pgx.component';
 import { PgxPremiumComponent } from './components/products/components/pgx-premium/pgx-premium.component';
 import { SingleGeneServiceComponent } from './components/products/components/single-gene-service/single-gene-service.component';
