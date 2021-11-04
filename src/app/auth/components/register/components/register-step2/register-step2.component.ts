ef735c911f1affd3b0bb5f1336bd7c9d4d7597d5
park.sy <park.sy@spmed.kr>
2021-11-04 09:29:21 +0900

    add signup second page function

--- src/app/auth/components/register/components/register-step2/register-step2.component.ts
+++ src/app/auth/components/register/components/register-step2/register-step2.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { MustMatch } from '../../../../../shared/classes/must-match.validator';
 
 @Component({
   selector: 'app-register-step2',
@@ -7,15 +9,21 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./register-step2.component.scss'],
 })
 export class RegisterStep2Component implements OnInit {
-  signUpForm2 = this.formBuilder.group({
-    fusername: ['', Validators.required],
-    fpassword: ['', Validators.required, Validators.minLength(4)],
-    repassword: ['', Validators.required],
-  });
-
+  signUpForm2 = this.formBuilder.group(
+    {
+      fusername: ['', Validators.required],
+      fpassword: ['', Validators.required, Validators.minLength(4)],
+      repassword: ['', Validators.required],
+    },
+    { validator: MustMatch('fpassword', 'repassword') }
+  );
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {}
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.signUpForm2.value;
+  }
 
   onSubmit() {
     if (this.signUpForm2.valid) {
