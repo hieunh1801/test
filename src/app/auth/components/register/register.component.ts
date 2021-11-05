b2779cfce161ca38fd800ecc00e055602e25726f
park.sy <park.sy@spmed.kr>
2021-11-05 15:17:17 +0900

    SignUp UI Update

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -3,13 +3,13 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { from, Subscription } from 'rxjs';
 import { MustMatch } from '../../../shared/classes/must-match.validator';
-import { DatePipe } from '@angular/common';
+import { DatePipe, Location } from '@angular/common';
 import {
   AuthService,
   IDCheckRequest,
   CustomerUserCreateRequest,
   CustomerUserCreateResponse,
-} from '../../auth.service';
+} from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -19,53 +19,25 @@ import {
 })
 export class RegisterComponent implements OnInit {
   // signUpForm: FormGroup;
-  signUpTempForm: FormGroup;
 
   genders: string[] = ['Male', 'Female', 'Other'];
   redirectToUrl = '#';
   formValue: any;
   agree: boolean;
-  step: number;
   parameter: number;
-  isShowStep1 = false;
-  isShowStep2 = false;
-  isShowStep3 = false;
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private route: ActivatedRoute,
-    private _datePipe: DatePipe
-  ) {
-    // this.createForm();
-  }
+  currentStep = 1;
 
-  ngOnInit(): void {
-    this.route.queryParams.subscribe((params) => {
-      this.step = +params.step;
-    });
+  signUpForm1 = this.formBuilder.group({
+    // add form 1 information here
+    totalAgree: [''],
+    termsAgree: ['', Validators.requiredTrue],
+    privacyAgree: ['', Validators.requiredTrue],
+  });
 
-    console.log(this.step);
-
-    if (this.step == 1) {
-      this.isShowStep1 = true;
-      this.isShowStep2 = false;
-      this.isShowStep3 = false;
-    } else if (this.step == 2) {
-      this.isShowStep1 = false;
-      this.isShowStep2 = true;
-      this.isShowStep3 = false;
-    } else if (this.step == 3) {
-      this.isShowStep1 = false;
-      this.isShowStep2 = false;
-      this.isShowStep3 = true;
-    }
-  }
-
-  signUpForm = this.formBuilder.group(
+  signUpForm2 = this.formBuilder.group(
     {
-      total_agree: [false],
-      terms_agree: [false, Validators.requiredTrue],
-      privacy_agree: [false, Validators.requiredTrue],
+      // TODO add form 2 information here
       fusername: ['', Validators.required],
       fpassword: [
         '',
@@ -76,62 +48,107 @@ export class RegisterComponent implements OnInit {
         ],
       ],
       repassword: ['', Validators.required],
-      fsirname: ['', Validators.required],
-      fgivenname: ['', Validators.required],
-      // validates date format yyyy-mm-dd
-      fdateofbirth: [
-        '',
-        [
-          Validators.required,
-          Validators.pattern(
-            /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/
-          ),
-        ],
-      ],
-      fgender: ['', Validators.required],
-      femail: ['', Validators.required, Validators.email],
-      fphone: ['', Validators.required],
     },
     { validator: MustMatch('fpassword', 'repassword') }
   );
 
+  signUpForm3 = this.formBuilder.group({
+    // TODO add form3 here
+    fsirname: ['', Validators.required],
+    fgivenname: ['', Validators.required],
+    // validates date format yyyy-mm-dd
+    fdateofbirth: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(
+          /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/
+        ),
+      ],
+    ],
+    fgender: ['', Validators.required],
+    femail: ['', Validators.required],
+    fphone: ['', Validators.required],
+  });
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private datePipe: DatePipe
+  ) {}
+
+  ngOnInit(): void {}
+
+  signUpForm = this.formBuilder.group({
+    termsAgree: ['', Validators.requiredTrue],
+    privacyAgree: ['', Validators.requiredTrue],
+    fusername: ['', Validators.required],
+    fpassword: [
+      '',
+      [Validators.required, Validators.minLength(4), Validators.maxLength(12)],
+    ],
+    fsirname: ['', Validators.required],
+    fgivenname: ['', Validators.required],
+    // validates date format yyyy-mm-dd
+    fdateofbirth: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(
+          /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/
+        ),
+      ],
+    ],
+    fgender: ['', Validators.required],
+    femail: ['', Validators.required],
+    fphone: ['', Validators.required],
+  });
+
   updateCheck() {
-    const formValue = this.signUpForm.value;
-    this.agree = formValue.total_agree;
+    const formValue = this.signUpForm1.value;
+    this.agree = formValue.totalAgree;
     this.agree = this.agree ? true : false;
 
-    this.signUpForm.patchValue({
-      terms_agree: this.agree,
-      privacy_agree: this.agree,
+    this.signUpForm1.patchValue({
+      termsAgree: this.agree,
+      privacyAgree: this.agree,
     });
   }
 
   onStep2() {
-    const formValue = this.signUpForm.value;
-    this.signUpTempForm.reset(formValue);
-    window.location.href = '/auth/register?step=2';
+    // TODO: because of form will submit (send a request to server) if we touch enter so we need to prevent this
+    // TODO: change current step is 2
+    this.currentStep = 2;
   }
   onStep3() {
-    window.location.href = '/auth/register?step=3';
+    this.currentStep = 3;
   }
 
   onSubmit(): void {
-    if (this.signUpForm.invalid) {
-      return;
-    }
+    this.signUpForm3.markAsDirty(); // ok i know this meaning : form input change
+    const form1Value = this.signUpForm1.value;
+    const form2Value = this.signUpForm2.value;
+    const form3Value = this.signUpForm3.value;
+    // const dob = this.datePipe.transform(form3Value.fdateofbirth, 'yyyy-MM-dd');
 
-    // let customerUserCreate: CustomerUserCreateRequest =new CustomerUserCreateRequest();
-    this.signUpForm.markAllAsTouched();
-    let dob = this._datePipe.transform(
-      this.signUpForm.value.fdateofbirth,
-      'yyyy-MM-dd'
-    );
-    console.log(dob);
-    // sampleCreate.receiptDate = this._datePipe.transform(sampleFormValue.sampleReceiptDate, 'yyyy-MM-dd');
+    this.signUpForm.patchValue({
+      // add all signUpForm field here
+      termsAgree: form1Value.termsAgree,
+      privacyAgree: form1Value.privacyAgree,
+      fusername: form2Value.fusername,
+      fpassword: form2Value.fpassword,
+      fsirname: form3Value.fsirname,
+      fgivenname: form3Value.fgivenname,
+      fdateofbirth: form3Value.fdateofbirth,
+      fgender: form3Value.fgender,
+      femail: form3Value.femail,
+      fphone: form3Value.fphone,
+    });
+    //console.log(this.signUpForm.value);
 
+    // call api sign up => I will do that after you done =))
     if (this.signUpForm.valid) {
-      this.signUpForm.reset();
-      window.location.href = '/auth/login';
     }
   }
 }
