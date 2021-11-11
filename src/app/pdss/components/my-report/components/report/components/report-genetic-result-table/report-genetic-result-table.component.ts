fea2dd57c030daf30dee799b399bc9a1b0fee98d
Nguyen Huu Hieu <hieunh1801@gmail.com>
2021-11-11 09:04:10 +0700

    (1) update report genetic result

--- src/app/pdss/components/my-report/components/report/components/report-genetic-result-table/report-genetic-result-table.component.ts
+++ src/app/pdss/components/my-report/components/report/components/report-genetic-result-table/report-genetic-result-table.component.ts
@@ -35,7 +35,8 @@ export class ReportGeneticResultTableComponent
   tableColumnList: string[] = [
     'geneSymbol',
     'relatedTestAlleles',
-    'variantGenotypeCode',
+    'variantGenotype',
+    'variantPhenotype',
     'variantPhenotypeSummary',
     'relatedDrugs',
   ];
