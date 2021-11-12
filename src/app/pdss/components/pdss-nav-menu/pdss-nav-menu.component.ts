caca3e20aa3af1bb42ac730e12c1e49428770358
Nguyen Huu Hieu <hieunh1801@gmail.com>
2021-11-12 15:33:45 +0700

    (1): pcdss menu animation

--- src/app/pdss/components/pdss-nav-menu/pdss-nav-menu.component.ts
+++ src/app/pdss/components/pdss-nav-menu/pdss-nav-menu.component.ts
@@ -1,3 +1,4 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { TokenStorageService } from '@shared/services/token-storage.service';
 
@@ -5,9 +6,19 @@ import { TokenStorageService } from '@shared/services/token-storage.service';
   selector: 'app-pdss-nav-menu',
   templateUrl: './pdss-nav-menu.component.html',
   styleUrls: ['./pdss-nav-menu.component.scss'],
+  animations: [
+    trigger('pdssNavMenuMobile', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('0ms', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [animate('200ms', style({ opacity: 0 }))]),
+    ]),
+  ],
 })
 export class PdssNavMenuComponent implements OnInit {
   isLogin = false;
+  isOpenMenuMobile = false;
   constructor(private tokenStorageService: TokenStorageService) {}
 
   ngOnInit(): void {
--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -3,13 +3,39 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  snsLinkList: SnsLink[] = [
+    {
+      title: 'facebook',
+      link: 'https://www.facebook.com/SPMEDkr/',
+      iconLink: '../../../../assets/common/facebook_icon.png',
+    },
 
-  constructor() { }
+    {
+      title: 'instagram',
+      link: 'https://www.instagram.com/spmed_official/',
+      iconLink: '../../../../assets/common/instagram_icon.png',
+    },
+    {
+      title: 'Naver',
+      link: 'https://blog.naver.com/spmed1602',
+      iconLink: '../../../../assets/common/naver_icon.png',
+    },
+    {
+      title: 'Kakao Talk',
+      link: '#',
+      iconLink: '../../../../assets/common/kakaotalk_icon.png',
+    },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
+  ngOnInit(): void {}
+}
 
+interface SnsLink {
+  title: string;
+  link: string;
+  iconLink: string;
 }
