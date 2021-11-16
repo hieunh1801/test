6e90c5525ece97fc9deb1686b7b6838ab2cc765c
park.sy <park.sy@spmed.kr>
2021-11-16 21:11:31 +0900

    add browser component

---
+++ src/app/pdss/components/browser/services/browser.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '@environment/environment';
+import { LanguageService } from '@shared/services/language.service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BrowserService {
+  private baseUrl = `${environment.gateway}/portal`;
+
+  constructor(
+    private languageService: LanguageService,
+    private httpClient: HttpClient
+  ) {}
+
+  search(searchRequest: SearchRequest): Observable<any> {
+    const url = `${this.baseUrl}/v${environment.version}/${this.languageService.currentLanguage}/knowledge-base/search-data-customer`;
+    const body = {
+      keyword: searchRequest.keyword,
+    };
+    return this.httpClient
+      .post<any>(url, body)
+      .pipe
+      //tap((data) => console.log(JSON.stringify(data))));
+      ();
+  }
+}
+
+export interface SearchRequest {
+  keyword: string;
+}
+
+export interface SearchResponse {
+  id: number;
+  name: string;
+  brandName: string;
+  summary: string;
+}
