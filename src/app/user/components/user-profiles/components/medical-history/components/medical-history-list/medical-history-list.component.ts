a6ccedbbbb68808b3523fc0635a924ad79aec906
Nguyen Huu Hieu <hieunh1801@gmail.com>
2021-11-25 14:03:37 +0700

    (1) refactor medical-history-list

--- src/app/user/components/user-profiles/components/medical-history/components/medical-history-list/medical-history-list.component.ts
+++ src/app/user/components/user-profiles/components/medical-history/components/medical-history-list/medical-history-list.component.ts
@@ -1,12 +1,12 @@
 import {
   Component,
+  EventEmitter,
   Input,
-  OnChanges,
   OnDestroy,
   OnInit,
-  SimpleChange,
-  SimpleChanges,
+  Output,
 } from '@angular/core';
+import { MedicalHistoryPutRequest } from '@user/services/user-medical-history.service';
 import { MedicalHistory } from '@user/services/user-profile.service';
 import { BehaviorSubject, Subscription } from 'rxjs';
 
@@ -17,27 +17,59 @@ import { BehaviorSubject, Subscription } from 'rxjs';
 })
 export class MedicalHistoryListComponent implements OnInit, OnDestroy {
   @Input() medicalHistoryList$ = new BehaviorSubject<MedicalHistory[]>([]);
-  @Input() dataSource: MedicalHistory[] = [];
+  @Input() mode$ = new BehaviorSubject<'VIEW' | 'EDIT'>(null);
+  @Input() showLess$ = new BehaviorSubject<boolean>(null);
 
-  showLess$ = new BehaviorSubject<boolean>(true);
+  @Output() showLessEvent = new EventEmitter<{ showLess: boolean }>();
+  @Output() cancelEvent = new EventEmitter();
+  @Output() deleteEvent = new EventEmitter<{ medicalHistoryId: number }>();
+  @Output() updateEvent = new EventEmitter<{
+    medicalHistoryId: number;
+    putRequest: MedicalHistoryPutRequest;
+  }>();
+
+  dataSource: MedicalHistory[] = [];
+  medicalHistoryIdEditSet = new Set();
+
   subscription$ = new Subscription();
 
   constructor() {}
 
+  ngOnInit(): void {
+    this.subscribeMedicalHistoryListChange();
+    this.subscribeModeChange();
+    this.subscribeShowLessChange();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription$.unsubscribe();
+  }
+
   reloadDataSource(): void {
     const medicalHistoryList = this.medicalHistoryList$.value;
+    const mode = this.mode$.value;
     const showLess = this.showLess$.value;
-    if (medicalHistoryList && medicalHistoryList.length > 0) {
-      if (showLess) {
-        this.dataSource = medicalHistoryList.slice(0, 3);
-      } else {
-        this.dataSource = medicalHistoryList;
-      }
+    if (
+      medicalHistoryList &&
+      medicalHistoryList.length > 0 &&
+      mode === 'VIEW' &&
+      showLess
+    ) {
+      this.dataSource = medicalHistoryList.slice(0, 3);
+    } else {
+      this.dataSource = medicalHistoryList;
     }
   }
 
   subscribeMedicalHistoryListChange(): void {
     const sub = this.medicalHistoryList$.subscribe(() => {
+      this.reloadDataSource();
+    });
+    this.subscription$.add(sub);
+  }
+
+  subscribeModeChange(): void {
+    const sub = this.mode$.subscribe(() => {
       this.reloadDataSource();
     });
     this.subscription$.add(sub);
@@ -50,13 +82,57 @@ export class MedicalHistoryListComponent implements OnInit, OnDestroy {
     this.subscription$.add(sub);
   }
 
-  ngOnInit(): void {
-    this.subscribeShowLessChange();
-    this.subscribeMedicalHistoryListChange();
+  isEditItem(medicalHistoryId: number): boolean {
+    return this.medicalHistoryIdEditSet.has(medicalHistoryId);
   }
 
-  ngOnDestroy(): void {
-    console.log('DESTROY');
-    this.subscription$.unsubscribe();
+  toggleEditItem(medicalHistoryId: number): void {
+    if (!medicalHistoryId) {
+      return;
+    }
+
+    const mSet = new Set(this.medicalHistoryIdEditSet);
+    if (mSet.has(medicalHistoryId)) {
+      mSet.delete(medicalHistoryId);
+    } else {
+      mSet.add(medicalHistoryId);
+    }
+    this.medicalHistoryIdEditSet = mSet;
+  }
+
+  updateItem({
+    medicalHistoryId,
+    putRequest,
+  }: {
+    medicalHistoryId: number;
+    putRequest: MedicalHistoryPutRequest;
+  }): void {
+    if (!medicalHistoryId) {
+      return;
+    }
+
+    this.updateEvent.emit({ medicalHistoryId, putRequest });
+  }
+
+  deleteItem(medicalHistoryId: number): void {
+    if (!medicalHistoryId) {
+      return;
+    }
+    this.deleteEvent.emit({ medicalHistoryId });
+  }
+
+  cancelClick(): void {
+    this.cancelEvent.emit();
+  }
+
+  showLessClick(): void {
+    this.showLessEvent.emit({
+      showLess: true,
+    });
+  }
+  showMoreClick(): void {
+    this.showLessEvent.emit({
+      showLess: false,
+    });
   }
 }
--- src/app/user/components/user-profiles/components/medical-history/medical-history.component.ts
+++ src/app/user/components/user-profiles/components/medical-history/medical-history.component.ts
@@ -1,9 +1,16 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { TranslateService } from '@ngx-translate/core';
+import {
+  ConfirmDialogComponent,
+  ConfirmDialogInput,
+  ConfirmDialogOutput,
+} from '@shared/components/confirm-dialog/confirm-dialog.component';
 import { MatSnackbarService } from '@shared/services/mat-snackbar.service';
 import { PageLoadingService } from '@shared/services/page-loading.service';
 import {
   MedicalHistoryPostRequest,
+  MedicalHistoryPutRequest,
   UserMedicalHistoryService,
 } from '@user/services/user-medical-history.service';
 import { MedicalHistory } from '@user/services/user-profile.service';
@@ -18,18 +25,27 @@ import { distinctUntilChanged, finalize } from 'rxjs/operators';
 export class MedicalHistoryComponent implements OnInit, OnDestroy {
   @Input() medicalHistoryList$ = new BehaviorSubject<MedicalHistory[]>([]);
 
-  medicalHistoryList: MedicalHistory[] = [];
+  mode$ = new BehaviorSubject<'VIEW' | 'EDIT' | 'ADD'>('VIEW');
+  showLess$ = new BehaviorSubject<boolean>(true);
   subscription$ = new Subscription();
 
-  MedicalHistoryMode = MedicalHistoryMode;
-  mode: MedicalHistoryMode = MedicalHistoryMode.VIEW; // default mode
+  medicalHistoryList: MedicalHistory[] = [];
 
   constructor(
     private userMedicalHistoryService: UserMedicalHistoryService,
     private pageLoadingService: PageLoadingService,
     private matSnackbarService: MatSnackbarService,
-    private translateService: TranslateService
+    private translateService: TranslateService,
+    private matDialog: MatDialog
   ) {}
+
+  ngOnInit(): void {
+    this.subscribeMedicalHistoryListChange();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription$.unsubscribe();
+  }
 
   subscribeMedicalHistoryListChange(): void {
     const sub = this.medicalHistoryList$
@@ -47,22 +63,13 @@ export class MedicalHistoryComponent implements OnInit, OnDestroy {
         const data = response?.data?.items || null;
         if (data) {
           this.medicalHistoryList$.next(data);
-          this.changeMode(MedicalHistoryMode.VIEW);
         }
       });
   }
 
-  ngOnInit(): void {
-    this.subscribeMedicalHistoryListChange();
-  }
-
-  ngOnDestroy(): void {
-    this.subscription$.unsubscribe();
-  }
-
-  changeMode(mode: MedicalHistoryMode): void {
+  changeMode(mode: 'VIEW' | 'EDIT' | 'ADD'): void {
     if (mode) {
-      this.mode = mode;
+      this.mode$.next(mode);
     }
   }
 
@@ -87,11 +94,73 @@ export class MedicalHistoryComponent implements OnInit, OnDestroy {
         }
       });
   }
-}
 
-enum MedicalHistoryMode {
-  VIEW = 'VIEW',
-  ADD = 'add',
-  EDIT = 'edit',
-  GRAPH = 'graph',
+  showLessEvent({ showLess }: { showLess: boolean }): void {
+    this.showLess$.next(showLess);
+  }
+
+  updateItem({
+    medicalHistoryId,
+    putRequest,
+  }: {
+    medicalHistoryId: number;
+    putRequest: MedicalHistoryPutRequest;
+  }): void {
+    if (medicalHistoryId) {
+      this.userMedicalHistoryService
+        .putUserMedicalHistory(medicalHistoryId, putRequest)
+        .subscribe({
+          next: (response) => {
+            const isSuccess = response?.status?.code === 'success';
+            if (isSuccess) {
+              this.reloadMedicalHistoryList();
+              this.matSnackbarService.openUpdateSuccess();
+            } else {
+              this.matSnackbarService.openUpdateFailed();
+            }
+          },
+          error: (error) => {
+            console.error(error);
+            this.matSnackbarService.openUpdateFailed();
+          },
+        });
+    }
+  }
+
+  deleteItem({ medicalHistoryId }: { medicalHistoryId: number }): void {
+    if (!medicalHistoryId) {
+      return;
+    }
+    const dialogInput: ConfirmDialogInput = {
+      title: this.translateService.instant(
+        'USER__USER_PROFILES__MEDICAL_HISTORY__MEDICAL_HISTORY_LIST_EDIT__CONFIRM_DELETE__CONFIRM_DELETE'
+      ),
+      content: this.translateService.instant(
+        'USER__USER_PROFILES__MEDICAL_HISTORY__MEDICAL_HISTORY_LIST_EDIT__CONFIRM_DELETE__ARE_YOUR_SURE_TO_DELETE'
+      ),
+    };
+    const dialogRef = this.matDialog.open(ConfirmDialogComponent, {
+      data: dialogInput,
+    });
+    dialogRef.afterClosed().subscribe((dialogOutput: ConfirmDialogOutput) => {
+      if (dialogOutput.action === 'yes') {
+        this.userMedicalHistoryService
+          .deleteUserMedicalHistory(medicalHistoryId)
+          .subscribe({
+            next: (response) => {
+              if (response?.status?.code === 'success') {
+                this.reloadMedicalHistoryList();
+                this.matSnackbarService.openDeleteSuccess();
+              } else {
+                this.matSnackbarService.openDeleteFailed();
+              }
+            },
+            error: (error) => {
+              console.error(error);
+              this.matSnackbarService.openUpdateFailed();
+            },
+          });
+      }
+    });
+  }
 }
