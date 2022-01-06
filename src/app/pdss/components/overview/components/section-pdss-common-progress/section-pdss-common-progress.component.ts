a5c92c8d06160277c7c798c7de8258a4de5971f8
Nguyen Huu Hieu <hieunh1801@gmail.com>
2022-01-06 14:47:48 +0700

    (1) pdss-overview > progress > add professional progress step

--- src/app/pdss/components/overview/components/section-pdss-common-progress/section-pdss-common-progress.component.ts
+++ src/app/pdss/components/overview/components/section-pdss-common-progress/section-pdss-common-progress.component.ts
@@ -7,35 +7,88 @@ import { marker } from '@biesbjerg/ngx-translate-extract-marker';
   styleUrls: ['./section-pdss-common-progress.component.scss'],
 })
 export class SectionPdssCommonProgressComponent implements OnInit {
+  mode: 'patient' | 'professional' = 'professional';
+
   constructor() {}
 
   ngOnInit(): void {}
 
-  processSteps = [
+  patientProcessSteps = [
     {
       stepIndex: '02',
-      stepContent: marker('PDSS__OVERVIEW__PROCESS_STEPS__STEP_02_CONTENT'), //'',
+      stepContent: marker(
+        'PDSS__OVERVIEW__PATIENT_PROCESS_STEPS__STEP_02_CONTENT'
+      ),
     },
     {
       stepIndex: '03',
-      stepContent: marker('PDSS__OVERVIEW__PROCESS_STEPS__STEP_03_CONTENT'), //'',
+      stepContent: marker(
+        'PDSS__OVERVIEW__PATIENT_PROCESS_STEPS__STEP_3_CONTENT'
+      ), //'',
     },
     {
       stepIndex: '04',
-      stepContent: marker('PDSS__OVERVIEW__PROCESS_STEPS__STEP_04_CONTENT'), //'',
+      stepContent: marker(
+        'PDSS__OVERVIEW__PATIENT_PROCESS_STEPS__STEP_4_CONTENT'
+      ),
     },
     {
       stepIndex: '05',
-      stepContent: marker('PDSS__OVERVIEW__PROCESS_STEPS__STEP_05_CONTENT'), //'',
+      stepContent: marker(
+        'PDSS__OVERVIEW__PATIENT_PROCESS_STEPS__STEP_5_CONTENT'
+      ),
     },
 
     {
       stepIndex: '06',
-      stepContent: marker('PDSS__OVERVIEW__PROCESS_STEPS__STEP_06_CONTENT'), //'',
+      stepContent: marker(
+        'PDSS__OVERVIEW__PATIENT_PROCESS_STEPS__STEP_6_CONTENT'
+      ),
     },
     {
       stepIndex: '07',
-      stepContent: marker('PDSS__OVERVIEW__PROCESS_STEPS__STEP_07_CONTENT'), //'',
+      stepContent: marker(
+        'PDSS__OVERVIEW__PATIENT_PROCESS_STEPS__STEP_7_CONTENT'
+      ),
     },
   ];
+
+  professionalProcessSteps = [
+    {
+      stepIndex: '02',
+      stepContent: marker(
+        'PDSS__OVERVIEW__PROFESSIONAL_PROCESS_STEPS__STEP_02_CONTENT'
+      ),
+    },
+
+    {
+      stepIndex: '03',
+      stepContent: marker(
+        'PDSS__OVERVIEW__PROFESSIONAL_PROCESS_STEPS__STEP_03_CONTENT'
+      ),
+    },
+    {
+      stepIndex: '04',
+      stepContent: marker(
+        'PDSS__OVERVIEW__PROFESSIONAL_PROCESS_STEPS__STEP_04_CONTENT'
+      ),
+    },
+
+    {
+      stepIndex: '05',
+      stepContent: marker(
+        'PDSS__OVERVIEW__PROFESSIONAL_PROCESS_STEPS__STEP_05_CONTENT'
+      ),
+    },
+    {
+      stepIndex: '06',
+      stepContent: marker(
+        'PDSS__OVERVIEW__PROFESSIONAL_PROCESS_STEPS__STEP_06_CONTENT'
+      ),
+    },
+  ];
+
+  changeMode(mode: 'patient' | 'professional'): void {
+    this.mode = mode;
+  }
 }
