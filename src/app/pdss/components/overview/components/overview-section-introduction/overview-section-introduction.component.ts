4591a99a462291451204f07c4f6f6562e3916e87
Nguyen Huu Hieu <hieunh1801@gmail.com>
2022-01-07 14:01:58 +0700

    (1) pdss i18n 50%

--- src/app/pdss/components/overview/components/overview-section-introduction/overview-section-introduction.component.ts
+++ src/app/pdss/components/overview/components/overview-section-introduction/overview-section-introduction.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-overview-section-introduction',
   templateUrl: './overview-section-introduction.component.html',
-  styleUrls: ['./overview-section-introduction.component.scss']
+  styleUrls: ['./overview-section-introduction.component.scss'],
 })
 export class OverviewSectionIntroductionComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/pdss/components/overview/components/overview-section-why-need/overview-section-why-need.component.ts
+++ src/app/pdss/components/overview/components/overview-section-why-need/overview-section-why-need.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { marker } from '@biesbjerg/ngx-translate-extract-marker';
 
 @Component({
   selector: 'app-overview-section-why-need',
   templateUrl: './overview-section-why-need.component.html',
-  styleUrls: ['./overview-section-why-need.component.scss']
+  styleUrls: ['./overview-section-why-need.component.scss'],
 })
 export class OverviewSectionWhyNeedComponent implements OnInit {
+  reasonList = [
+    {
+      title: marker('PDSS__OVERVIEW__WHY_NEED__REASONS__1__TITLE'),
+      content: marker('PDSS__OVERVIEW__WHY_NEED__REASONS__1__CONTENT'),
+      imgUrl:
+        'https://image.freepik.com/free-vector/drug-allergy-abstract-concept-vector-illustration-triggers-drug-allergies-risk-factors-medicine-side-effect-remedy-intolerance-test-allergic-disease-symptom-treatment-abstract-metaphor_335657-1556.jpg',
+    },
+    {
+      title: marker('PDSS__OVERVIEW__WHY_NEED__REASONS__2__TITLE'),
+      content: marker('PDSS__OVERVIEW__WHY_NEED__REASONS__2__CONTENT'),
+      imgUrl:
+        'https://image.freepik.com/free-vector/healthy-people-carrying-different-icons_53876-43069.jpg',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
