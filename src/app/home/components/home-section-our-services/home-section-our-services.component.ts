d6baa4e0a99b5ed7b01ba1e0f056dee9d9685c24
Nguyen Huu Hieu <hieunh1801@gmail.com>
2022-01-07 11:52:09 +0700

    (1) home i18n

--- src/app/home/components/home-section-our-services/home-section-our-services.component.ts
+++ src/app/home/components/home-section-our-services/home-section-our-services.component.ts
@@ -1,15 +1,131 @@
 import { Component, OnInit } from '@angular/core';
+import { marker } from '@biesbjerg/ngx-translate-extract-marker';
 
 @Component({
   selector: 'app-home-section-our-services',
   templateUrl: './home-section-our-services.component.html',
-  styleUrls: ['./home-section-our-services.component.scss']
+  styleUrls: ['./home-section-our-services.component.scss'],
 })
 export class HomeSectionOurServicesComponent implements OnInit {
+  servicesList = [
+    {
+      title: marker('HOME__SECTION_OUR_SERVICE__SERVICES__1__TITLE'),
+      imageUrl:
+        'https://image.freepik.com/free-vector/organic-flat-customer-support-illustration_23-2148899173.jpg',
+      contents: [
+        marker('HOME__SECTION_OUR_SERVICE__SERVICES__1__CONTENTS__1'),
+        marker('HOME__SECTION_OUR_SERVICE__SERVICES__1__CONTENTS__2'),
+        marker('HOME__SECTION_OUR_SERVICE__SERVICES__1__CONTENTS__3'),
+        marker('HOME__SECTION_OUR_SERVICE__SERVICES__1__CONTENTS__4'),
+        marker('HOME__SECTION_OUR_SERVICE__SERVICES__1__CONTENTS__5'),
+        marker('HOME__SECTION_OUR_SERVICE__SERVICES__1__CONTENTS__6'),
+      ],
+      routeActions: [
+        {
+          title: marker(
+            'HOME__SECTION_OUR_SERVICE__SERVICES__1__ROUTE_ACTION__1'
+          ),
+          routeLink: '',
+          callback: () => {},
+        },
+        {
+          title: marker(
+            'HOME__SECTION_OUR_SERVICE__SERVICES__1__ROUTE_ACTION__2'
+          ),
+          routeLink: '',
+          callback: () => {},
+        },
+      ],
+    },
+    {
+      title: marker('HOME__SECTION_OUR_SERVICE__SERVICES__2__TITLE'),
+      imageUrl:
+        'https://image.freepik.com/free-vector/organic-flat-customer-support-illustration_23-2148899174.jpg',
+      contents: [
+        marker('HOME__SECTION_OUR_SERVICE__SERVICES__2__CONTENTS__1'),
+        marker('HOME__SECTION_OUR_SERVICE__SERVICES__2__CONTENTS__2'),
+        marker('HOME__SECTION_OUR_SERVICE__SERVICES__2__CONTENTS__3'),
+        marker('HOME__SECTION_OUR_SERVICE__SERVICES__2__CONTENTS__4'),
+        marker('HOME__SECTION_OUR_SERVICE__SERVICES__2__CONTENTS__5'),
+        marker('HOME__SECTION_OUR_SERVICE__SERVICES__2__CONTENTS__6'),
+      ],
+      routeActions: [
+        {
+          title: marker(
+            'HOME__SECTION_OUR_SERVICE__SERVICES__2__ROUTE_ACTION__1'
+          ),
+          routeLink: '',
+          callback: () => {},
+        },
+        {
+          title: marker(
+            'HOME__SECTION_OUR_SERVICE__SERVICES__2__ROUTE_ACTION__2'
+          ),
+          routeLink: '',
+          callback: () => {},
+        },
+      ],
+    },
+    {
+      title: marker('HOME__SECTION_OUR_SERVICE__SERVICES__3__TITLE'),
+      imageUrl:
+        'https://image.freepik.com/free-vector/two-business-partners-handshaking_74855-6685.jpg',
+      contents: [
+        marker('HOME__SECTION_OUR_SERVICE__SERVICES__3__CONTENTS__1'),
+        marker('HOME__SECTION_OUR_SERVICE__SERVICES__3__CONTENTS__2'),
+        marker('HOME__SECTION_OUR_SERVICE__SERVICES__3__CONTENTS__3'),
+        marker('HOME__SECTION_OUR_SERVICE__SERVICES__3__CONTENTS__4'),
+        marker('HOME__SECTION_OUR_SERVICE__SERVICES__3__CONTENTS__5'),
+        marker('HOME__SECTION_OUR_SERVICE__SERVICES__3__CONTENTS__6'),
+      ],
+      routeActions: [
+        {
+          title: marker(
+            'HOME__SECTION_OUR_SERVICE__SERVICES__3__ROUTE_ACTION__1'
+          ),
+          routeLink: '',
+          callback: () => {},
+        },
+        {
+          title: marker(
+            'HOME__SECTION_OUR_SERVICE__SERVICES__3__ROUTE_ACTION__2'
+          ),
+          routeLink: '',
+          callback: () => {},
+        },
+      ],
+    },
+    {
+      title: marker('HOME__SECTION_OUR_SERVICE__SERVICES__4__TITLE'),
+      imageUrl:
+        'https://image.freepik.com/free-vector/illustration-healthy-lifestyle_53876-28533.jpg',
+      contents: [
+        marker('HOME__SECTION_OUR_SERVICE__SERVICES__4__CONTENTS__1'),
+        marker('HOME__SECTION_OUR_SERVICE__SERVICES__4__CONTENTS__2'),
+        marker('HOME__SECTION_OUR_SERVICE__SERVICES__4__CONTENTS__3'),
+        marker('HOME__SECTION_OUR_SERVICE__SERVICES__4__CONTENTS__4'),
+        marker('HOME__SECTION_OUR_SERVICE__SERVICES__4__CONTENTS__5'),
+        marker('HOME__SECTION_OUR_SERVICE__SERVICES__4__CONTENTS__6'),
+      ],
+      routeActions: [
+        {
+          title: marker(
+            'HOME__SECTION_OUR_SERVICE__SERVICES__4__ROUTE_ACTION__1'
+          ),
+          routeLink: '',
+          callback: () => {},
+        },
+        {
+          title: marker(
+            'HOME__SECTION_OUR_SERVICE__SERVICES__4__ROUTE_ACTION__2'
+          ),
+          routeLink: '',
+          callback: () => {},
+        },
+      ],
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/home/components/home-section-why-choose-us/home-section-why-choose-us.component.ts
+++ src/app/home/components/home-section-why-choose-us/home-section-why-choose-us.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { marker } from '@biesbjerg/ngx-translate-extract-marker';
 
 @Component({
   selector: 'app-home-section-why-choose-us',
   templateUrl: './home-section-why-choose-us.component.html',
-  styleUrls: ['./home-section-why-choose-us.component.scss']
+  styleUrls: ['./home-section-why-choose-us.component.scss'],
 })
 export class HomeSectionWhyChooseUsComponent implements OnInit {
+  reasonList = [
+    {
+      title: marker('HOME__SECTION_WHY_CHOOSE_US__SLOGANS__1'),
+      imgUrl: 'https://image.flaticon.com/icons/png/512/1253/1253680.png',
+    },
+    {
+      title: marker('HOME__SECTION_WHY_CHOOSE_US__SLOGANS__2'),
+      imgUrl: 'https://image.flaticon.com/icons/png/512/1245/1245362.png',
+    },
+    {
+      title: marker('HOME__SECTION_WHY_CHOOSE_US__SLOGANS__3'),
+      imgUrl: 'https://image.flaticon.com/icons/png/512/1253/1253337.png',
+    },
+    {
+      title: marker('HOME__SECTION_WHY_CHOOSE_US__SLOGANS__4'),
+      imgUrl: 'https://image.flaticon.com/icons/png/512/1256/1256486.png',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
