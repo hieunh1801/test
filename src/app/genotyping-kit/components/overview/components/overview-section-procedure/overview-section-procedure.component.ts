6066caced31de6f8ce29d46da687cd8792d6e677
Nguyen Huu Hieu <hieunh1801@gmail.com>
2022-01-10 12:05:50 +0700

    (1) genotyping kit

--- src/app/genotyping-kit/components/overview/components/overview-section-procedure/overview-section-procedure.component.ts
+++ src/app/genotyping-kit/components/overview/components/overview-section-procedure/overview-section-procedure.component.ts
@@ -1,15 +1,28 @@
+import { marker } from '@biesbjerg/ngx-translate-extract-marker';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-overview-section-procedure',
   templateUrl: './overview-section-procedure.component.html',
-  styleUrls: ['./overview-section-procedure.component.scss']
+  styleUrls: ['./overview-section-procedure.component.scss'],
 })
 export class OverviewSectionProcedureComponent implements OnInit {
+  procedureList: Procedure[] = [
+    {
+      title: marker('GENOTYPING_KIT__OVERVIEW__SECTIONS__PROCEDURES__1'),
+      imgUrl: '/assets/images/genotyping-kit-procedure-1.png',
+    },
+    {
+      title: marker('GENOTYPING_KIT__OVERVIEW__SECTIONS__PROCEDURES__2'),
+      imgUrl: '/assets/images/genotyping-kit-procedure-2.png',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
+}
 
-  ngOnInit(): void {
-  }
-
+interface Procedure {
+  title: string;
+  imgUrl: string;
 }
--- src/app/genotyping-kit/components/overview/components/overview-section-where-to-use/overview-section-where-to-use.component.ts
+++ src/app/genotyping-kit/components/overview/components/overview-section-where-to-use/overview-section-where-to-use.component.ts
@@ -1,15 +1,18 @@
+import { marker } from '@biesbjerg/ngx-translate-extract-marker';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-overview-section-where-to-use',
   templateUrl: './overview-section-where-to-use.component.html',
-  styleUrls: ['./overview-section-where-to-use.component.scss']
+  styleUrls: ['./overview-section-where-to-use.component.scss'],
 })
 export class OverviewSectionWhereToUseComponent implements OnInit {
+  contentList: string[] = [
+    marker('GENOTYPING_KIT__OVERVIEW__SECTIONS__WHERE_TO_USE__CONTENTS__1'),
+    marker('GENOTYPING_KIT__OVERVIEW__SECTIONS__WHERE_TO_USE__CONTENTS__2'),
+    marker('GENOTYPING_KIT__OVERVIEW__SECTIONS__WHERE_TO_USE__CONTENTS__3'),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
