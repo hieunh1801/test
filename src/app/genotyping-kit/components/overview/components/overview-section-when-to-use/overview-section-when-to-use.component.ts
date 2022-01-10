06ec6f3c4f8016592501251f692cbcea7d8817a1
Nguyen Huu Hieu <hieunh1801@gmail.com>
2022-01-10 10:58:41 +0700

    (1) when to use genotyping kit

--- src/app/genotyping-kit/components/overview/components/overview-section-when-to-use/overview-section-when-to-use.component.ts
+++ src/app/genotyping-kit/components/overview/components/overview-section-when-to-use/overview-section-when-to-use.component.ts
@@ -1,15 +1,18 @@
+import { marker } from '@biesbjerg/ngx-translate-extract-marker';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-overview-section-when-to-use',
   templateUrl: './overview-section-when-to-use.component.html',
-  styleUrls: ['./overview-section-when-to-use.component.scss']
+  styleUrls: ['./overview-section-when-to-use.component.scss'],
 })
 export class OverviewSectionWhenToUseComponent implements OnInit {
+  reasonList: string[] = [
+    marker('GENOTYPING_KIT__OVERVIEW__SECTIONS__WHEN_TO_USE__REASONS__1'),
+    marker('GENOTYPING_KIT__OVERVIEW__SECTIONS__WHEN_TO_USE__REASONS__2'),
+    marker('GENOTYPING_KIT__OVERVIEW__SECTIONS__WHEN_TO_USE__REASONS__3'),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
