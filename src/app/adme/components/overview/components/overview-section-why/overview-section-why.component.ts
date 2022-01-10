d52793524d650d07236f17e1703743d866890107
Nguyen Huu Hieu <hieunh1801@gmail.com>
2022-01-10 16:00:04 +0700

    (1) adme > overview > why

--- src/app/adme/components/overview/components/overview-section-why/overview-section-why.component.ts
+++ src/app/adme/components/overview/components/overview-section-why/overview-section-why.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { marker } from '@biesbjerg/ngx-translate-extract-marker';
 
 @Component({
   selector: 'app-overview-section-why',
   templateUrl: './overview-section-why.component.html',
-  styleUrls: ['./overview-section-why.component.scss']
+  styleUrls: ['./overview-section-why.component.scss'],
 })
 export class OverviewSectionWhyComponent implements OnInit {
+  reasonList: string[] = [
+    marker('ADME__OVERVIEW__SECTIONS__WHY__REASON_LIST__1'),
+    marker('ADME__OVERVIEW__SECTIONS__WHY__REASON_LIST__2'),
+    marker('ADME__OVERVIEW__SECTIONS__WHY__REASON_LIST__3'),
+    marker('ADME__OVERVIEW__SECTIONS__WHY__REASON_LIST__4'),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
