23c8f04555c4b4277cee969fbf791daebaf09eb7
Nguyen Huu Hieu <hieunh1801@gmail.com>
2022-01-11 10:12:23 +0700

    (1) human recombinant enzyme > overview > procedure

--- src/app/recombinant-enzyme/components/overview/components/overview-section-procedure/overview-section-procedure.component.ts
+++ src/app/recombinant-enzyme/components/overview/components/overview-section-procedure/overview-section-procedure.component.ts
@@ -1,15 +1,51 @@
+import { marker } from '@biesbjerg/ngx-translate-extract-marker';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-overview-section-procedure',
   templateUrl: './overview-section-procedure.component.html',
-  styleUrls: ['./overview-section-procedure.component.scss']
+  styleUrls: ['./overview-section-procedure.component.scss'],
 })
 export class OverviewSectionProcedureComponent implements OnInit {
+  procedureList = [
+    {
+      content: marker(
+        'RECOMBINANT_ENZYME__OVERVIEW__SECTIONS__PROCEDURE__PROCEDURE_LIST__1'
+      ),
+      imgUrl: '/assets/images/human_recombinant_enzyme_procedure_1.jpg',
+    },
+    {
+      content: marker(
+        'RECOMBINANT_ENZYME__OVERVIEW__SECTIONS__PROCEDURE__PROCEDURE_LIST__2'
+      ),
+      imgUrl: '/assets/images/human_recombinant_enzyme_procedure_2.jpg',
+    },
+    {
+      content: marker(
+        'RECOMBINANT_ENZYME__OVERVIEW__SECTIONS__PROCEDURE__PROCEDURE_LIST__3'
+      ),
+      imgUrl: '/assets/images/human_recombinant_enzyme_procedure_3.jpg',
+    },
+    {
+      content: marker(
+        'RECOMBINANT_ENZYME__OVERVIEW__SECTIONS__PROCEDURE__PROCEDURE_LIST__4'
+      ),
+      imgUrl: '/assets/images/human_recombinant_enzyme_procedure_4.jpg',
+    },
+    {
+      content: marker(
+        'RECOMBINANT_ENZYME__OVERVIEW__SECTIONS__PROCEDURE__PROCEDURE_LIST__5'
+      ),
+      imgUrl: '/assets/images/human_recombinant_enzyme_procedure_5.jpg',
+    },
+    {
+      content: marker(
+        'RECOMBINANT_ENZYME__OVERVIEW__SECTIONS__PROCEDURE__PROCEDURE_LIST__6'
+      ),
+      imgUrl: '/assets/images/human_recombinant_enzyme_procedure_6.jpg',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
