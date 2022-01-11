d1032da2005478f781e0f513d26eda2b8bec4419
Nguyen Huu Hieu <hieunh1801@gmail.com>
2022-01-11 08:55:28 +0700

    (1) human recombinant enzyme > overview > feature and benefits

--- src/app/recombinant-enzyme/components/overview/components/overview-section-feature-and-benefit/overview-section-feature-and-benefit.component.ts
+++ src/app/recombinant-enzyme/components/overview/components/overview-section-feature-and-benefit/overview-section-feature-and-benefit.component.ts
@@ -1,15 +1,57 @@
+import { marker } from '@biesbjerg/ngx-translate-extract-marker';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-overview-section-feature-and-benefit',
   templateUrl: './overview-section-feature-and-benefit.component.html',
-  styleUrls: ['./overview-section-feature-and-benefit.component.scss']
+  styleUrls: ['./overview-section-feature-and-benefit.component.scss'],
 })
 export class OverviewSectionFeatureAndBenefitComponent implements OnInit {
+  contentList = [
+    {
+      content: marker(
+        'RECOMBINANT_ENZYME__OVERVIEW__SECTIONS__FEATURE_AND_BENEFIT__CONTENT_LIST__1'
+      ),
+      imgUrl:
+        '/assets/images/human_recombinant_enzyme_feature_and_benefit_content_1.jpg',
+    },
+    {
+      content: marker(
+        'RECOMBINANT_ENZYME__OVERVIEW__SECTIONS__FEATURE_AND_BENEFIT__CONTENT_LIST__2'
+      ),
+      imgUrl:
+        '/assets/images/human_recombinant_enzyme_feature_and_benefit_content_2.jpg',
+    },
+    {
+      content: marker(
+        'RECOMBINANT_ENZYME__OVERVIEW__SECTIONS__FEATURE_AND_BENEFIT__CONTENT_LIST__3'
+      ),
+      imgUrl:
+        '/assets/images/human_recombinant_enzyme_feature_and_benefit_content_3.jpg',
+    },
+    {
+      content: marker(
+        'RECOMBINANT_ENZYME__OVERVIEW__SECTIONS__FEATURE_AND_BENEFIT__CONTENT_LIST__4'
+      ),
+      imgUrl:
+        '/assets/images/human_recombinant_enzyme_feature_and_benefit_content_4.jpg',
+    },
+    {
+      content: marker(
+        'RECOMBINANT_ENZYME__OVERVIEW__SECTIONS__FEATURE_AND_BENEFIT__CONTENT_LIST__5'
+      ),
+      imgUrl:
+        '/assets/images/human_recombinant_enzyme_feature_and_benefit_content_5.jpg',
+    },
+    {
+      content: marker(
+        'RECOMBINANT_ENZYME__OVERVIEW__SECTIONS__FEATURE_AND_BENEFIT__CONTENT_LIST__6'
+      ),
+      imgUrl:
+        '/assets/images/human_recombinant_enzyme_feature_and_benefit_content_6.jpg',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
