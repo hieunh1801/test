5f1b2e5d7c407daa74dd172ef8b9e2e355a581bc
Nguyen Huu Hieu <hieunh1801@gmail.com>
2022-01-19 17:10:56 +0700

    (1) animation for homepage

---
+++ src/app/@animation/directives/animate-after-appear.directive.spec.ts
@@ -0,0 +1,8 @@
+import { AnimateAfterAppearDirective } from './animate-after-appear.directive';
+
+describe('AnimateAfterAppearDirective', () => {
+  it('should create an instance', () => {
+    const directive = new AnimateAfterAppearDirective();
+    expect(directive).toBeTruthy();
+  });
+});
