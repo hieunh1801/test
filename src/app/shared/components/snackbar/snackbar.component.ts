5f33b6620dbab64dad3ad00f56803fb0e8276c50
Nguyen Huu Hieu <hieunh1801@gmail.com>
2022-01-20 10:48:26 +0700

    (1) custom snackbar

--- src/app/shared/components/snackbar/snackbar.component.ts
+++ src/app/shared/components/snackbar/snackbar.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-snackbar',
   templateUrl: './snackbar.component.html',
-  styleUrls: ['./snackbar.component.scss']
+  styleUrls: ['./snackbar.component.scss'],
 })
 export class SnackbarComponent implements OnInit {
+  constructor(@Inject(MAT_SNACK_BAR_DATA) public data: SnackbarInput) {}
 
-  constructor() { }
+  ngOnInit(): void {}
+}
 
-  ngOnInit(): void {
-  }
-
+export interface SnackbarInput {
+  message?: string;
+  action?: string;
 }
