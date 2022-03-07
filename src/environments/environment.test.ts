fd3deabb32d4a18cc361b0bdeedcbbb46a156642
Nguyen Huu Hieu <hieunh1801@gmail.com>
2022-03-07 08:16:50 +0700

    (1) add test environment

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,10 @@
+// run with --configuration staging
+
+import { commonEnv } from './environment.common';
+
+const env: Partial<typeof commonEnv> = {
+  host: 'http://localhost:61126',
+  gateway: 'https://dev-gwapi.spmed.kr/api-gateway/v1.0',
+};
+
+export const environment = { ...commonEnv, ...env };
